3f8d2153248fc8f6a185ee102d4681d95cff9a2f
Renan Ribeiro De Carvalho <20171ENEL0365@cefetes.br>
2019-08-16 17:37:42 -0300

    Comentário

--- Acomp2/InsertionSort.cs
+++ Acomp2/InsertionSort.cs
@@ -4,7 +4,21 @@
     {
         public void Sort(int[] E, int n)
         {
-            throw new System.NotImplementedException();
+            //throw new System.NotImplementedException();
+            for(int i = 0; i < n; i++)
+            {
+                int eleito = E[i];
+                int j = i - 1;
+                while((j >= 0) && (eleito < E[j]))
+                {
+                    E[j + 1] = E[j];
+                    j = j - 1;
+                }
+                if(j != i)
+                {
+                    E[j + 1] = eleito;
+                }
+            }
         }
     }
 }
